137018441cb730775573a9629814438bd2ef8aba
Andrey Yatsuk <hi@yatsukav.com>
2017-03-31 23:37:55 +0300

    COR:+ added initial functionality

--- src/test/java/com/yatsukav/colorsort/AppTest.java
+++ src/test/java/com/yatsukav/colorsort/AppTest.java
@@ -1,34 +1,32 @@
 package com.yatsukav.colorsort;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
+
+public class AppTest {
+
+    @Test
+    public void bmpToArray() throws URISyntaxException, IOException {
+        URL path = AppTest.class.getClassLoader().getResource("sample.bmp").toURI().toURL();
+        int[] result = App.loadBmp(path);
+        System.out.println(Arrays.toString(result));
+
+        URI path2 = new File("test.bmp").toURI();
+        App.saveToBmp(result, path2, 16, 2);
+
+        Arrays.sort(result);
+        URI path3 = new File("test2.bmp").toURI();
+        App.saveToBmp(result, path3, 16, 2);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
+    @Test
+    public void arrayToBmp() {
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
-    }
 }
