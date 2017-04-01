91ecd190929abdf28c58de1c3fc6509edb7e193a
Andrey Yatsuk <hi@yatsukav.com>
2017-04-01 10:42:13 +0300

    [+] added movie making tools and libs

---
+++ src/main/java/com/yatsukav/colorsort/movie/MovieMaker.java
@@ -0,0 +1,21 @@
+package com.yatsukav.colorsort.movie;
+
+import javax.media.MediaLocator;
+import java.net.MalformedURLException;
+import java.util.Vector;
+
+public class MovieMaker {
+
+    public static void makeVideo(String fileName) throws MalformedURLException {
+        Vector<String> imgLst = new Vector<>(); // paths
+
+        JpegImagesToMovie imageToMovie = new JpegImagesToMovie();
+        MediaLocator oml;
+        if ((oml = JpegImagesToMovie.createMediaLocator(fileName)) == null) {
+            System.err.println("Cannot build media locator from: " + fileName);
+            System.exit(0);
+        }
+        int interval = 50;
+        imageToMovie.doIt(1920, 1080, (1000 / interval), imgLst, oml);
+    }
+}
